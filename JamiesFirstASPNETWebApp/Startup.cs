12bfcdaeefb0620cf72efae5e6d22d070896ea22
jamiescromar <jamiescromar@gmail.com>
2021-01-20 11:12:53 -0700

    Add project files.

---
+++ JamiesFirstASPNETWebApp/Startup.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JamiesFirstASPNETWebApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //when we use this we will not need to type in the index.html at the end of the URL
+            app.UseDefaultFiles();
+            //By moving index.html to wwwroot it makes it so that we can see the file at localhost/index.html
+            app.UseStaticFiles();
+
+        }
+    }
+}
